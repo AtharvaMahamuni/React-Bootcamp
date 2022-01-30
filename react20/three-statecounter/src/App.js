19b827343d4b01899032436cf0a6ca215bc85354
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-01-30 19:24:21 +0530

    components, states/hooks, props and lifecycle

---
+++ react20/three-statecounter/src/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import "./App.css"
+
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  return(
+    <div className='App'>
+      <header>
+        <h1>Counter app using State/hooks</h1>
+      </header>
+      <h2>Current value of count is {count}</h2>
+      <button onClick={() => setCount(0)}>Reset Counter</button>
+      <button onClick={() => count>=10 ? "": setCount(count+1)}>Increase Counter</button>
+      <button onClick={() => count>0? setCount(count-1):""}>Decrease Counter</button>
+    </div>
+  );
+}
+
+
+export default App;
