5d4630c8b6a2d9b1f11c6c3abf2f1e84f97a029e
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-04-01 21:13:48 +0530

    Theme Toggler

---
+++ react20/five-themswitcher/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThemeToggler from "./ThemeToggler";
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Theme toggler</h1>
+            <ThemeToggler />
+        </header>
+    )
+}
+
+export default Header;
